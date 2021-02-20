80061586873cf559d28c0fa92a3fa9ec907f1ecb
Parham Javadi <pjfooeve09@yahoo.com>
2021-02-19 16:36:13 -0800

    commit tree 8a2faf4d02e2de7a956b8cf48b9f48bc25713886

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
-end
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.actors.map {|e| "#{e.first_name} #{e.last_name}"}
+    end
+  end
